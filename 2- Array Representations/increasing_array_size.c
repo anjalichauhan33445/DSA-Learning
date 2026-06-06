858cae208f3f8e35d782f3827024de41b3e987be
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-06-06 15:32:05 +0530

    Increase array size (Heap using pointers)

---
+++ 2- Array Representations/increasing_array_size.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int *p, *q;
+    p = (int *)malloc( 5 * sizeof(int));
+    p[0] = 1; p[1] = 3; p[2] = 5; p[3] = 7; p[4] = 9;
+
+    q = (int *)malloc( 10 * sizeof(int));
+    for(int i = 0; i<5;i++) 
+        q[i] = p[i];
+
+   
+    for(int i = 0;i<5;i++) 
+        printf("%d\n",q[i]);
+
+    free(p);
+    p = q;
+    q = NULL;
+
+    printf("--------------\n");
+
+    for(int i = 0;i<5;i++)
+        printf("%d\n",p[i]);
+
+
+
+    return 0;
+}
