8caf31531d9cdeb28a95590e8600656e38f3de50
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-06-12 20:42:36 +0530

    Strivers

--- Strivers/patterns.cpp
+++ Strivers/patterns.cpp
@@ -249,6 +249,33 @@ void pattern20(int n){
 }
 
 
+void pattern21(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            if(i==0|| j==0|| i==n-1 || j==n-1){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+void pattern22(int n){
+    for(int i = 0; i < 2*n-1; i++){
+        for(int j = 0; j<2*n-1; j++){
+            int top = i;
+            int left = j;
+            int bottom = (2*n-2) - i;
+            int right = (2*n-2) - j;
+            cout<< n-min(min(top,bottom), min(left,right));
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     // pattern7(5);
    
@@ -264,7 +291,8 @@ int main(){
     //pattern17(4);
     //pattern18(5);
     //pattern19(5);
-    pattern20(5);
+    //pattern20(5);
+    pattern22(4);
 
 
 }
