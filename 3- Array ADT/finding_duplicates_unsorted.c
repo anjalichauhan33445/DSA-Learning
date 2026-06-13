4504b535ea73096a0d6d15566756969501b5a714
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-06-13 20:07:51 +0530

    Finding Duplicates: Unsorted- Using Hash table or bit set

--- 3- Array ADT/finding_duplicates_unsorted.c
+++ 3- Array ADT/finding_duplicates_unsorted.c
@@ -2,8 +2,12 @@
 
 int main(){
     int arr[10] = {8,3,6,4,6,5,8,2,6,7};
+    int arr1[10] = {8,3,6,4,6,5,8,2,6,7};
     //Method-1
     int n = 10;
+    int H[9] = {0};
+    
+
     for(int i = 0; i<n-1; i++){
         int count = 1;
         for(int j = i+1; j<n; j++){
@@ -14,5 +18,18 @@ int main(){
         }
         if(count>1) printf("%d appeared %d times\n", arr[i],count);
     }
+
+    //Method-2
+    printf("\nMethod 2\n");
+    for(int i = 0; i<n; i++){
+        H[arr1[i]]++;
+    }
+
+    for(int i = 0; i<9; i++){
+        if(H[i]>1){
+            printf("%d appeared %d times\n",i,H[i]);
+        }
+    }
+
     return 0;
 } 
