f9eafdd6864ec0ab3fe9fd74664ab45d76e91835
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-06-13 19:54:04 +0530

    Finding Duplicates Unsorted array- Method1

---
+++ 3- Array ADT/finding_duplicates_unsorted.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int arr[10] = {8,3,6,4,6,5,8,2,6,7};
+    //Method-1
+    int n = 10;
+    for(int i = 0; i<n-1; i++){
+        int count = 1;
+        for(int j = i+1; j<n; j++){
+            if(arr[i]==arr[j] && arr[i]!=-1){
+                count++;
+                arr[j] = -1;
+            }
+        }
+        if(count>1) printf("%d appeared %d times\n", arr[i],count);
+    }
+    return 0;
+} 
