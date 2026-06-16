562f52de573588da7e91e295cd3384f24569c637
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-06-16 15:44:23 +0530

    Implement function to count number of words

---
+++ 4- Strings/count_words.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int count_words(char* s){
+
+    int words = 0;
+    for(int i = 0; s[i]!='\0'; i++){
+        if((i==0 && s[i]!=' ') || (s[i]!=' ' && s[i-1]==' ')) words++;
+    }
+    return words;
+}
+
+int main(){
+    char s[]  = "HEHE HAHA  HA  HA   HA";
+    printf("No of words in the string: %d\n",count_words(s));
+    return 0;
+}
+
---
+++ 4- Strings/tempCodeRunnerFile.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+// int count_words(char* s){
+
+//     int words = 0;
+//     for(int i = 0; s[i]!='\0'; i++){
+//         if((s[i]==0 && s[i]!=' ') || (s[i]!=' ' && s[i-1]==' ')) words++;
+//     }
+//     return words;
+// }
+
+// int main(){
+//     char s[]  = "HEHE HAHA  HA  HA   HA";
+//     printf("No of words in the string: %d\n",count_words(s));
+//     return 0;
+// }
