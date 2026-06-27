d015f9c149fa22ad1ccbc8cf065f0f683a446396
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-06-27 14:21:37 +0530

    Implement Optimal Approach for Maximum Subarray Average

--- Questions/Arrays/MaxSubArrAvgI.java
+++ Questions/Arrays/MaxSubArrAvgI.java
@@ -2,11 +2,23 @@
 
 // Brute Force approach
 class MaxSubArrAvgI {
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
         int[] arr = {1,12,-5,-6,50,3};
-        int n = arr.length;
         int k = 4;
-        double max_sum = 0;
+        double max_avg_sum = optimal(arr, k);
+        System.out.println("The Maximum Average subarray is "+max_avg_sum);
+    }
+    static double bruteForce(int[] arr, int k) {
+        
+        int n = arr.length;
+   
+        if(n==1) {
+
+            return (double)arr[0]/k;
+            }
+   
+        double max_sum = Double.NEGATIVE_INFINITY;
         int j;
         for(int i = 0; i<=n-k; i++){
             j = i;
@@ -19,7 +31,24 @@ class MaxSubArrAvgI {
             
             
         }
-        System.out.println(max_sum);
+        return max_sum;
         
+    }
+
+    static double optimal(int[] arr, int k){
+        int n = arr.length;
+        
+        int sum = 0;
+        for(int i = 0; i<k; i++){
+            sum+=arr[i];
+        }
+        int maxSum = sum;
+
+        for(int i = 1; i<=n-k; i++){
+            sum = sum-arr[i-1]+arr[i+k-1];
+            maxSum = Math.max(maxSum,sum);
+        }
+
+        return (double)maxSum/k;
     }
 }
