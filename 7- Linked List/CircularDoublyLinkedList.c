d5396c33597b4124e7f5dec1b07d5b1516509094
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-07-01 16:58:55 +0530

    Create and Display Circular Doubly Linked List

--- 7- Linked List/CircularDoublyLinkedList.c
+++ 7- Linked List/CircularDoublyLinkedList.c
@@ -9,16 +9,39 @@ struct Node{
 
 
 void create(int arr[], int n){
+    struct Node *last;
     head = (struct Node *)malloc(sizeof(struct Node));
     head->data = arr[0];
     head->prev = head;
     head->next = head;
+    last = head;
+
+    for(int i = 1; i<n ; i++){
+        struct Node *t =(struct Node *)malloc(sizeof(struct Node));
+        t->data = arr[i];
+        t->prev = last;
+        t->next = last->next;
+        last->next = t;
+        head->prev = t;
+        last = t;
+    }
     
-    
+}
+
+
+void Display(){
+    struct Node *p = head;
+    do{
+        printf("%d\n",p->data);
+        p = p->next;
+    }
+    while(p!=head);
+
 }
 
 int main(){
     int arr[] = {10,20,30,40,50};
     create(arr,5);
+    Display();
     return 0;
 }
