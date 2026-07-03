5b3de7493880b64a7c642196e90a6557b55970e3
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-07-03 14:18:57 +0530

    Delete a Node in Circular Doubly Linked List

--- 7- Linked List/CircularDoublyLinkedList.c
+++ 7- Linked List/CircularDoublyLinkedList.c
@@ -92,10 +92,49 @@ void Display(){
 
 }
 
+int Delete(int pos){
+    
+    int x = -1;
+    if(pos<0 || pos>Length()) return x;
+    
+
+    if(pos==1){
+        if(head->next == head){
+            x = head->data;
+            free(head);
+            head = last = NULL;
+        }
+        else{
+            //can use last pointer as well... 
+            struct Node *p = head;
+            head = head->next;
+            head->prev = p->prev;
+            p->prev->next = head;
+            x = p->data;
+            free(p);
+        }
+
+    }
+    else{
+        struct Node *p = head;
+        for(int i = 0; p && i<pos-1; i++){
+            p = p->next;
+        }
+        p->prev->next = p->next;
+        p->next->prev = p->prev;
+        x = p->data;
+        free(p);
+    }
+    return x;
+}
+
 int main(){
     int arr[] = {10,20,30,40,50};
     create(arr,5);
-    Insert(5,100);
+    Display();
+    printf("\n\n");
+    printf("Element Deleted :%d\n",(Delete(2)));
+    printf("\nList:\n");
     Display();
     return 0;
 }
