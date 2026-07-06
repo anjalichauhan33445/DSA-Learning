93d988e5ccfea44bacb6019b9cd9834f694bc7e1
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-07-06 14:35:20 +0530

    Convert Infix to Postfix expressions having operators +, -, *, /

---
+++ 8-Stack/infix_to_postfix.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//Conversion for Basic expressions including operators like +,*,-,/
+
+struct Stack{
+    int Top;
+    int size;
+    char *S;
+};
+
+void push(struct Stack *st, char x){
+    if(st->Top == st->size-1) return;
+    st->Top++;
+    st->S[st->Top] = x;
+}
+
+char pop(struct Stack *st){
+    char x = '\0';
+    if(st->Top==-1) return x;
+    x = st->S[st->Top];
+    st->Top--;
+    return x;
+}
+
+int isEmpty(struct Stack st){
+    return st.Top==-1?1:0;
+}
+
+int pre(char ch){
+    
+    if(ch=='+' || ch == '-') return 1;
+    if(ch=='*' || ch == '/') return 2;
+    return 0;
+
+}
+
+int isOperand(char ch){
+    if(ch =='+' || ch =='-' || ch =='*' || ch =='/') return 0;
+    return 1; 
+}
+
+char stackTop(struct Stack st){
+    if(st.Top==-1) return '\0';
+    return st.S[st.Top];
+}
+char *convert(char *exp){
+    struct Stack st;
+    st.Top = -1;
+    st.size = strlen(exp);
+    st.S = (char *)malloc(st.size * sizeof(char));
+    char *postfix = (char *)malloc((strlen(exp)+1)*sizeof(char));
+    int i = 0,j = 0;
+
+    while(exp[i]!='\0'){
+        if(isOperand(exp[i])){
+            postfix[j++] = exp[i++];
+        }
+        else{
+            if(pre(exp[i])>pre(stackTop(st))){
+                push(&st,exp[i]);
+                i++;
+            }
+            else{
+                postfix[j++] = pop(&st);
+            }
+            
+        }
+    }
+    while(!isEmpty(st)){
+        postfix[j++] = pop(&st);
+    }
+    postfix[j] = '\0';
+    free(st.S);
+    return postfix;
+}
+
+int main(){
+    char exp[] = "a+b*c-d/e";
+    char *ans  = convert(exp);
+    printf("%s\n",ans);
+    free(ans);
+    return 0;
+}
