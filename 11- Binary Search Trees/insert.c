1e6162a7d287105e38706b2190fb3f099d3f79b2
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-07-12 21:01:30 +0530

    feat: implement Iterative Function for insertion in a BST

--- 11- Binary Search Trees/insert.c
+++ 11- Binary Search Trees/insert.c
@@ -5,26 +5,61 @@ struct Node{
     struct Node *lchild;
     int data;
     struct Node *rchild;
-};
+} *root = NULL;
 
-
-struct Node *RInsert(struct Node *p, int key){
+void Insert(int key){
     struct Node *t;
+    struct Node *r = NULL;
+    struct Node *p = root;
 
-    if(p==NULL){
-        t = (struct Node *)malloc(sizeof(struct Node));
+    if(root==NULL){
+        t = (struct Node*)malloc(sizeof(struct Node));
         t->data = key;
         t->lchild = t->rchild = NULL;
-        return t;
+        root = t;
+        return;
     }
-    if(key<p->data){
-        p->lchild = RInsert(p->lchild,key);
+    while(p!=NULL){
+        r = p;
+        if(key < p->data){
+            p = p->lchild;
+        }
+        else if(key>p->data){
+            p = p->rchild;
+        }
+        else 
+            return;
     }
-    else if(key>p->data){
-        p->rchild = RInsert(p->rchild,key);
-    }
-    return p;
+
+    t = (struct Node*)malloc(sizeof(struct Node));
+    t->data = key;
+    t->lchild = t->rchild = NULL;
+
+    if(r->data<t->data) r->rchild = t;
+    else r->lchild = t;
+
+
+
 }
+
+
+// struct Node *RInsert(struct Node *p, int key){
+//     struct Node *t;
+
+//     if(p==NULL){
+//         t = (struct Node *)malloc(sizeof(struct Node));
+//         t->data = key;
+//         t->lchild = t->rchild = NULL;
+//         return t;
+//     }
+//     if(key<p->data){
+//         p->lchild = RInsert(p->lchild,key);
+//     }
+//     else if(key>p->data){
+//         p->rchild = RInsert(p->rchild,key);
+//     }
+//     return p;
+// }
 
 
 void InOrder(struct Node *p){
@@ -37,12 +72,14 @@ void InOrder(struct Node *p){
 }
 
 int main(){
-    struct Node *root = NULL;
-    root = RInsert(root,30);
-    RInsert(root,20);
-    RInsert(root,40);
-    RInsert(root,10);
-
+    
+    Insert(30);
+    Insert(20);
+    Insert(40);
+    Insert(10);
+    Insert(25);
+    Insert(35);
+    Insert(50);
     InOrder(root);
 
     return 0;
