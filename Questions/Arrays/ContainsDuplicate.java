c39c79f11b8480a720f1990aa2b2e42fb14b3f1c
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-07-21 15:39:05 +0530

    Solve leetcode 217- Contains Duplicate

---
+++ Questions/Arrays/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class ContainsDuplicate {
+    static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i<nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        if(containsDuplicate(nums)){
+            System.out.println("The array contains duplicate");
+        }
+        else{
+            System.out.println("The array doesn't contain duplicate");
+        }
+    }
+}
