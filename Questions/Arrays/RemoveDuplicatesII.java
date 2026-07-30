4e9142443fd00c9f922257f2e29792d8613fe793
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-07-30 21:41:34 +0530

    Solve: Remove Duplicates II from a Sorted ARray

---
+++ Questions/Arrays/RemoveDuplicatesII.java
@@ -0,0 +1,17 @@
+class RemoveDuplicatesII{
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3};
+        System.out.println("Output: "+removeDuplicates(nums));
+    }
+    static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int num: nums){
+            if(i==0 || i==1 || num!=nums[i-2]){
+                nums[i] = num;
+                i++;
+            }
+        }
+        return i;
+        
+    }
+}
