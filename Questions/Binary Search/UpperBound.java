04bab0b734c74ff093a64944f63de74947d5a59d
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-08-09 14:41:57 +0530

    Implement Upper Bound using Binary Search

---
+++ Questions/Binary Search/UpperBound.java
@@ -0,0 +1,29 @@
+public class UpperBound {
+
+    static int upperBound(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]>target){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return low;
+        
+    }
+    public static void main(String[] args) {
+       int[] arr= {2, 3, 7, 10, 11, 11, 25};
+       int target = 9;
+       System.out.println("Output: "+upperBound(arr,target));
+    }
+
+
+}
+
+
+
+    
