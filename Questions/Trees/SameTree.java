d9b9b9440668596db9333e3cbe8f32f35f12334b
Anjali Chauhan <anjalichauhan33445@gmail.com>
2026-09-22 20:47:13 +0530

    Solve LeetCode: Is Same Tree

---
+++ Questions/Trees/SameTree.java
@@ -0,0 +1,43 @@
+public class SameTree {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null || q == null) {
+            return p == q;
+        }
+
+        return p.val == q.val
+                && isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        System.out.println(isSameTree(p, q));
+    }
+}
